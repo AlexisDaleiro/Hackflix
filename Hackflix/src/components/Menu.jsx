2d5d0b4d389b4f080c5fe6f0df87b5126f89d316
GiancarloFornara <gfornara5@gmail.com>
2025-06-11 19:06:37 -0300

    LOGO + FUNC
    Subi la imagen y agregue una funcion para la barra

---
+++ Hackflix/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Navbar.css';
+
+
+const Navbar = () => {
+    return(
+<nav className="navbar"> <div className="navbar-brand">
+        <img src="Hackflix\src\assets\logo.png" alt="" />
+      </div>
+      <div> <a href="/usuario">INICIAR SESIÓN</a></div>
+         
+        
+      </nav>
+    )
+};
+export default Navbar
