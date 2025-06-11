2721b41b24e20819a653a0de080bfdf193b22e7e
Alexis Daleiro <alexisdaleiro@gmail.com>
2025-06-11 18:24:46 -0300

    Update ApiInicio.jsx

--- Hackflix/src/components/ApiInicio.jsx
+++ Hackflix/src/components/ApiInicio.jsx
@@ -2,22 +2,22 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function (props) {
-  const [personaje, setPersonaje] = useState(null);
+  const [pelicula, setPelicula] = useState(null);
 
   useEffect(() => {
     const getPersonaje = async () => {
       const response = await axios.get(
         `https://api.themoviedb.org/3/search/movie?query=Star%20Wars&api_key=51f5870eb2fb3938f2ca55d7c2326f86`
       );
-      setPersonaje(response.data);
+      setPelicula(response.data);
       console.log(response.data);
     };
     getPersonaje();
   }, []);
   return (
-    personaje && (
+    pelicula && (
       <div className="row justify-content-center">
-        {personaje.results.map((item) => (
+        {pelicula.results.map((item) => (
           <div key={item.id} className="col-md-2">
             <div className="m-1">
               <img
