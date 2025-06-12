329aaf91bc65f944f910ba3d63bb7441b74d7fa7
Angie-Porro <angiemautone18@gmail.com>
2025-06-12 17:51:15 -0300

    Angie cambios

--- Hackflix/src/App.jsx
+++ Hackflix/src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import Api from "./components/ApiInicio";
 import Navbar from "./components/Menu";
 import Banner from "./components/BannerPrincipal";
+import { Angie } from "./components/Angie";
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
-
+      <Angie />
       <Api />
     </>
   );
