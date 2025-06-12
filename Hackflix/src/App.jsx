19630e7b8dfabd4e1e9eda2ba7f0163f0accda93
Angie-Porro <angiemautone18@gmail.com>
2025-06-12 17:44:01 -0300

    Update App.jsx

--- Hackflix/src/App.jsx
+++ Hackflix/src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Api from "./components/ApiInicio";
-import Buscador from "./components/Buscador";
-import Estrellas from "./components/Estrellas";
 import Navbar from "./components/Menu";
 import Banner from "./components/BannerPrincipal";
 function App() {
