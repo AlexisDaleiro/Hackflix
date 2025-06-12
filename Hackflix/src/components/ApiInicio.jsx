84f138a7893060f9452b3d8eb64fd156a3a90298
Alexis Daleiro <alexisdaleiro@gmail.com>
2025-06-12 15:32:53 -0300

    Filtro completo

--- Hackflix/src/components/ApiInicio.jsx
+++ Hackflix/src/components/ApiInicio.jsx
@@ -1,37 +1,42 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import Estrellas from "../components/Estrellas";
 import "react-horizontal-scrolling-menu/dist/styles.css";
-import { Rating } from "react-simple-star-rating";
-import Estrellas from "./Estrellas";
 
 export default function ApiInicio() {
   const [pelicula, setPelicula] = useState(null);
   const [filtro, setFiltro] = useState(0);
 
   useEffect(() => {
-    const getPelicula = async () => {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/discover/movie?api_key=51f5870eb2fb3938f2ca55d7c2326f86`
-      );
+    const getPeliculas = async () => {
+      let url = "";
+
+      if (filtro === 0) {
+        url = `https://api.themoviedb.org/3/movie/popular?api_key=51f5870eb2fb3938f2ca55d7c2326f86`;
+      } else {
+        const voteAverage = filtro * 2;
+        url = `https://api.themoviedb.org/3/discover/movie?api_key=51f5870eb2fb3938f2ca55d7c2326f86&vote_average.gte=${voteAverage}`;
+      }
+
+      const response = await axios.get(url);
       setPelicula(response.data);
-      console.log(response.data);
     };
-    getPelicula();
-  }, []);
+
+    getPeliculas();
+  }, [filtro]);
 
   return (
     pelicula && (
-      <div>
-        <div className="mb-4">
-          <Estrellas filtro={filtro} setFiltro={setFiltro} />
-        </div>
+      <div className="container py-3">
+        <Estrellas filtro={filtro} setFiltro={setFiltro} />
+
         <div className="d-flex overflow-auto flex-nowrap">
           {pelicula.results
-            .filter((item) => item.vote_average / 2 >= filtro)
+            .filter((item) => item.poster_path !== null)
             .map((item) => (
               <div key={item.id} className="me-3">
                 <div className="m-1">
-                  <a href="">
+                  <a href="" className="text-decoration-none text-white">
                     <img
                       src={`https://image.tmdb.org/t/p/w200${item.poster_path}`}
                       alt={item.title}
@@ -43,7 +48,7 @@ export default function ApiInicio() {
                       className="rounded-3"
                     />
                     <div style={{ textAlign: "center", marginTop: "5px" }}>
-                      ⭐ {(item.vote_average / 2).toFixed(1)}
+                      ⭐ {(item.vote_average / 2).toFixed(0)}
                     </div>
                   </a>
                 </div>
