905329204a501f21fdc6844972324c9365b4c421
Alexis Daleiro <alexisdaleiro@gmail.com>
2025-06-12 17:51:12 -0300

    prueba

--- Hackflix/src/App.jsx
+++ Hackflix/src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 import Api from "./components/ApiInicio";
 import Navbar from "./components/Menu";
 import Banner from "./components/BannerPrincipal";
+import componentenuevo from "./components/componentenuevo";
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
-
+      <componentenuevo />
       <Api />
     </>
   );
