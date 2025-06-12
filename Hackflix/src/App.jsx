3a2c3c2444235e767da12a117799ae47d49e6018
Alexis Daleiro <alexisdaleiro@gmail.com>
2025-06-12 17:56:31 -0300

    Update App.jsx

--- Hackflix/src/App.jsx
+++ Hackflix/src/App.jsx
@@ -1,5 +1,8 @@
+import { useState } from "react";
 import "./App.css";
 import Api from "./components/ApiInicio";
+import Buscador from "./components/Buscador";
+import Estrellas from "./components/Estrellas";
 import Navbar from "./components/Menu";
 import Banner from "./components/BannerPrincipal";
 
